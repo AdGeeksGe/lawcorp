08dc9e90fa0bc318adc721e17cede983a95a9be4
AdGeeksGe <n.javakhishvili@adgeeks.ge>
2025-01-17 10:58:44 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <?php include './components/header.php'; ?>
+        <?php include './components/head.php'; ?>
     </head>
     
 <body>
-   
+    <?php include './components/header.php'; ?>
 </body>
 </html>
